6ca2e0b6a940104a861f030dc44f7027ce4ce4b6
chaggysen <sen.wang@mail.mcgill.ca>
2020-09-22 20:17:57 -0400

    fix import

--- EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplicationTests.java
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplicationTests.java
@@ -1,7 +1,7 @@
 package ca.mcgill.ecse321.eventregistration;
 
 import org.junit.jupiter.api.Test;
-import org.junit.Ignore;
+import org.junit.jupiter.api.Ignore;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
