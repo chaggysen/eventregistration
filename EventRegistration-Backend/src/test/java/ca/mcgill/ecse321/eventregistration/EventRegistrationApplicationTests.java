1d17110b02a8127df4a06a38039f23b18294482b
chaggysen <sen.wang@mail.mcgill.ca>
2020-09-22 20:20:27 -0400

    commented out the tests

--- EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplicationTests.java
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/EventRegistrationApplicationTests.java
@@ -1,15 +1,13 @@
 package ca.mcgill.ecse321.eventregistration;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Ignore;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EventRegistrationApplicationTests {
 
-	@Test
-	@Ignore
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
